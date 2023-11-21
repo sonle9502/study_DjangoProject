7a7db8c01a59e9e75dbbe36e5b490c6a1922d5aa
sonle9502 <114076484+sonle9502@users.noreply.github.com>
2023-11-21 10:16:01 +0900

    Update settings.py

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -126,11 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/base.css/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "todoapp/static")
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
