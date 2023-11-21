6774ebaaba5242102e450344fd026151661da3f3
SonLv1994 <soncuc182304@gmail.com>
2023-11-21 23:37:43 +0900

    update html css

--- todoapp/static/js/script.js
+++ todoapp/static/js/script.js
@@ -1,2 +1,15 @@
 // script.js
-alert("Hello, this is a Django project!");
+//alert("Hello, this is a Django project!");
+
+document.addEventListener('DOMContentLoaded', function() {
+    // This function will be executed when the DOM is fully loaded
+
+    // Get the element with the ID 'change-text'
+    var myElement = document.getElementById('change-text');
+
+    // Check if the element exists
+    if (myElement) {
+        // Change the text content of the element
+        myElement.textContent = "Hello from script.js!";
+    }
+});
