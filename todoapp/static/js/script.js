b580b3f4e74bbb961dbc61654ea32b2726f8e056
SonLv1994 <soncuc182304@gmail.com>
2023-11-23 12:30:09 +0900

    Displat description When mouseover

--- todoapp/static/js/script.js
+++ todoapp/static/js/script.js
@@ -13,3 +13,38 @@ document.addEventListener('DOMContentLoaded', function() {
         myElement.textContent = "Hello from script.js!";
     }
 });
+
+
+
+// JavaScriptで確認ボタンにイベントリスナーを追加
+document.addEventListener("DOMContentLoaded", function() {
+    const kakuninButtons = document.querySelectorAll(".kakuninButton");
+    kakuninButtons.forEach(function(button) {
+        button.addEventListener("mouseover", function(e) {
+            const taskId = button.getAttribute("data-task-id");
+            const popup = document.getElementById("popup_" + taskId);
+            
+            // Show the popup
+            popup.style.display = "block";
+
+            // ポップアップの位置を設定
+            // popup.style.left = e.clientX + "px"; // Add 10 pixels to create some spacing
+            // popup.style.top = e.clientY - 70  + "px";
+            popup.style.left = e.clientX + "px";
+            popup.style.top = e.clientY + window.scrollY + "px"; 
+
+            // ポップアップを表示
+            //popup.style.display = "block";
+
+        });
+
+        button.addEventListener("mouseout", function(e) {
+            const taskId = button.getAttribute("data-task-id");
+            const popup = document.getElementById("popup_" + taskId);
+            
+            // ポップアップを表示
+            popup.style.display = "none";
+
+        });
+    });
+});
